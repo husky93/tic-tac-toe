07e71e6a4cbd11e428880b9a8549d476a8ab7159
Maciej Sroka <sroka.maciej93@gmail.com>
2022-07-01 16:22:08 +0200

    Setup the player and game objects

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+const Player = (name) => { 
+    return {name}
+}
+
 const gameBoard = (() => {
     let board = [[null,null,null], 
                 [null,null,null],
                 [null,null,null]];
     return {board};
 })();
+
+const game = (() => {
+    return {};
+})();
+
+const playerOne = Player('Player 1');
+const playerTwo = Player('Player 2');
